de512b4e5da1726879fe1e1689d9968375dbb0b6
TheODDYSEY <mosessabz07@gmail.com>
2023-11-01 20:43:27 +0300

    created index in types folder and finalizing on scrapin

--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 import * as cheerio from "cheerio";
-import { extractPrice } from "../utils";
+import { extractCurrency, extractDescription, extractPrice } from "../utils";
 
 export async function scrapeAmazonProduct(url: string) {
     if (!url) return;
@@ -29,15 +29,54 @@ export async function scrapeAmazonProduct(url: string) {
         const $ = cheerio.load(response.data);
 
         // scrape the product title
-        const title = $("#productTitle").text().trim();
+        const title = $('#productTitle').text().trim();
         const currentPrice = extractPrice(
-            $('.priceToPay span.a-price-whole'),
-            $('a.size.base.a-color-price'),
-            $('.a-button-selected .a-color-base'),
-            $('.a-price.a-text-price')
+          $('.priceToPay span.a-price-whole'),
+          $('.a.size.base.a-color-price'),
+          $('.a-button-selected .a-color-base'),
         );
+    
+        const originalPrice = extractPrice(
+          $('#priceblock_ourprice'),
+          $('.a-price.a-text-price span.a-offscreen'),
+          $('#listPrice'),
+          $('#priceblock_dealprice'),
+          $('.a-size-base.a-color-price')
+        );
+
+        const outOfStock = $('#availability span').text().trim().toLowerCase() === 'currently unavailable';
         
-        console.log({title,currentPrice})
+        const images= 
+        $('#imgBlkFront').attr('data-a-dynamic-image') || 
+        $('#landingImage').attr('data-a-dynamic-image') ||
+        '{}'
+
+        const imageUrls = Object.keys(JSON.parse(images));
+
+        const currency = extractCurrency ($('a-price-symbol'))
+        const discountRate = $('.savingsPercentage').text().replace(/[-%]/g, "");
+
+        const description = extractDescription($)
+      // Construct data object with scraped information
+      const data = {
+        url,
+        currency: currency || '$',
+        image: imageUrls[0],
+        title,
+        currentPrice: Number(currentPrice) || Number(originalPrice),
+        originalPrice: Number(originalPrice) || Number(currentPrice),
+        priceHistory: [],
+        discountRate: Number(discountRate),
+        category: 'category',
+        reviewsCount:100,
+        stars: 4.5,
+        isOutOfStock: outOfStock,
+        description,
+        lowestPrice: Number(currentPrice) || Number(originalPrice),
+        highestPrice: Number(originalPrice) || Number(currentPrice),
+        averagePrice: Number(currentPrice) || Number(originalPrice),
+      }
+      console.log(data)
     } catch (error: any) {
         throw new Error(`Failed to scrape product: ${error.message}`);
     }
