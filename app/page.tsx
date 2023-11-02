21f2b69ee09a8283615bc993352608a901d89303
TheODDYSEY <mosessabz07@gmail.com>
2023-11-03 01:44:06 +0300

    Designing product details page

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,13 @@ import React from 'react'
 import Image from 'next/image'
 import SearchBar from '@/components/SearchBar'
 import HeroCarousel from '@/components/HeroCarousel'
+import { getAllProducts } from '@/lib/actions'
+import ProductCard from '@/components/ProductCard'
 
-const Home = () => {
+const Home = async () => {
+  const allProducts = await getAllProducts();
+
+
   return (
     <>
       <section className='px-6 md:px-20 py-24 border-2 '>
@@ -36,12 +41,13 @@ const Home = () => {
       </section>
       <section className='trending-section'>
         <h2 className='section-text'>Trending</h2>
-        <div className='flex flex-wrap gap-x-8 gap-y-16'>
-          {['Apple Iphone15','Book','Sneakers'].map
-          ((product)=>(
-            <div>{product}</div>
+
+        <div className="flex flex-wrap gap-x-8 gap-y-16">
+          {allProducts?.map((product) => (
+            <ProductCard key={product._id} product={product} />
           ))}
         </div>
+        
       </section>
 
     </>
