37527ceae3edf2ba16fb843a00733cdd5a658006
TheODDYSEY <mosessabz07@gmail.com>
2023-12-28 16:54:33 +0300

    fixed max duration limit

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -5,7 +5,7 @@ import { scrapeAmazonProduct } from "@/lib/scraper";
 import { getAveragePrice, getEmailNotifType, getHighestPrice, getLowestPrice } from "@/lib/utils";
 import { NextResponse } from "next/server";
 
-export const maxDuration = 300; // This function can run for a maximum of 300 seconds
+export const maxDuration = 10; // This function can run for a maximum of 300 seconds
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
